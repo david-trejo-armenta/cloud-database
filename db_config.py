0e26b47bdcb8e1db298e13b49a4aee1ae1b9308a
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-06-08 17:26:21 -0600

    git config

---
+++ db_config.py
@@ -0,0 +1,14 @@
+""" Flask configuration"""
+
+from flask_pymongo import pymongo
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+client = pymongo.MongoClient(
+    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.rvnou.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+    )
+db = client.test
+
