e12da652812f76d08248fed380a9a85b7bb051d6
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-06-08 22:19:08 -0600

    Changes made to config.py

--- db_config.py
+++ db_config.py
@@ -7,8 +7,10 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
+DB_NAME
 client = pymongo.MongoClient(
-    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.rvnou.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@example.rvnou.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
     )
-db = client.Badges
 
+    #Down here you need to add the collection name
+db = client.db_example
