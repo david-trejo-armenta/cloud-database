4d90f2e70d375a46967022d05c0782d742b32a65
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-06-09 21:43:38 -0600

    special ops

--- main.py
+++ main.py
@@ -12,10 +12,35 @@ app=Flask(__name__)
 
 api=Api(app)
 
+@app.route('/all/adults/')
+def get_adults():
+    response = list(database.db.Badges.find({'age': {"$gte": 21}}))
 
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+@app.route('/all/projection/')
+def get_name_and_age():
+    response = list(database.db.Badges.find({'age': {"$gte": 21}}, {'name':1, 'age': 1}))
+    
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+@app.route('/all/kids/')
+def get_kids():
+    response = list(database.db.Badges.find({'age': {"$lt": 21}}, {'name':1, 'age': 1}))
+    
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
 
 api.add_resource(Badge,'/new','/<string:by>=<string:data>/')
-api.add_resource(Badges, '/delete/all/')
+api.add_resource(Badges, '/all/')
 
 if __name__ == '__main__':
-    app.run(load_dotenv=True,)
+    app.run(load_dotenv=True)
